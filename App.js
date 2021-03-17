5ce5354a51c039e5095b325eb3fb584b1858418c
danielvazquezguerra <daniel.vazquezguerra@gmail.com>
2021-03-17 21:51:47 +0100

    modal insert

--- App.js
+++ App.js
@@ -1,56 +1,61 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { Children, useState, useEffect } from 'react';
-import { StyleSheet, Text, View, FlatList, Dimensions } from 'react-native';
+import { StyleSheet, View, ImageBackground, Text, Modal, Button } from 'react-native';
 
 
 
 export default function App() {
 
-  const [ users, setUsers ] = useState([]);
-  const [ loading, setLoading ] = useState(true);
-
-  useEffect(() => {
-    
-    fetch('https://jsonplaceholder.typicode.com/users')
-      .then(response => response.json())
-      .then(data => {
-        setUsers(data)
-        setLoading(false)
-      })
-    
-
-  }, [])
+  const [modal, setModal] = useState(false);
 
 
-  if (loading) {
 
-    return <View style={styles.loading}><Text>Loading</Text></View>
-
-  }
 
   return (
 
     <View style={styles.container}>
 
-      <StatusBar style="auto" />
+      <Modal
 
-        <Text style={styles.title}>FlatList</Text>
+        animationType='slide'
+        transparent={true}
+        visible={modal}
+      
+      >
 
-        <View style={styles.Wrapper}>
+        <View
+          style={styles.center}
+        >
 
-            <FlatList
+          <View style={styles.content}>
 
-              data={users}
+            <Text>Soy un modal</Text>
+
+            <Button
+
+              title='Cerrar modal'
+              onPress={()=>setModal(!modal)}
+              style={styles.button}
             
-              keyExtractor={ item => String(item.id)}
-              renderItem={({ item }) => <Text style={styles.flatlist}>{item.name}</Text>}
-              
-              />
+            />
+
+      
+
+
+          </View>
+
+
 
         </View>
 
+      </Modal>
 
+      <Button 
 
+        title='Abrir modal'
+        onPress={()=>{setModal(!modal)}}
+        style={styles.button}
+      />
 
     </View>   
 
@@ -59,48 +64,27 @@ export default function App() {
 
 const styles = StyleSheet.create({
 
-    container: {
+    content: {
+
+      backgroundColor
+
+
+
+    },
+
+      container: {
       flex: 1,
       backgroundColor: 'yellow',
       alignItems: 'center',
       justifyContent: 'center',
     },
 
-    title: {
+    center: {
 
-      fontSize: 30,
-      fontWeight: 'bold',
-      borderBottomColor: 'black',
-      borderBottomWidth: 2,
-      marginBottom: 20
-    },
-
-    flatlist:{
-
-      height: 40,
-      paddingLeft: 20,
-    },
-
-    Wrapper: {
-      display: 'flex',
-      width: '100%',
-      height: '50%',
-      padding: 20,
-    },
-
-    section: {
-      fontSize: 20,
-      fontWeight: 'bold',
-      marginBottom: 10,
-    },
-
-    loading: {
-      display: 'flex',
-      justifyContent: 'center',
+      flex: 1,
       alignItems: 'center',
-      width: '100%',
-      height: '100%',
-      backgroundColor: 'yellow',
+      justifyContent: 'center',
 
-  }
+    },
+
 })
