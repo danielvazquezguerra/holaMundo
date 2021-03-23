dcade5a490b82841eabc2ac6281259fa85e0bc26
danielvazquezguerra <daniel.vazquezguerra@gmail.com>
2021-03-23 01:13:54 +0100

    location no funciona

--- App.js
+++ App.js
@@ -1,90 +1,54 @@
-import { StatusBar } from 'expo-status-bar';
-import React, { Children, useState, useEffect } from 'react';
-import { StyleSheet, View, ImageBackground, Text, Modal, Button } from 'react-native';
+
+import React, {useEffect, useState} from 'react';
+import * as Location from 'expo-location';
+import * as Permissions from 'expo-permissions'
+import { Alert, StyleSheet, Text, View } from 'react-native';
 
 
 
 export default function App() {
 
-  const [modal, setModal] = useState(false);
+  const buscaLocation = async () => {
+
+    console.log(Permissions);
+
+    let { status } = await Permissions.askAsync(Permissions.LOCATION)
+
+
+    if ( status !== 'granted' ) {
+
+      return Alert.alert('No tenemos permisos')
+    }
+
+    const location = await Location.getCurrentPositionAsync({})
+    console.log(location);
+  }
 
 
 
+  useEffect(() => {
+
+    buscaLocation()
+
+  }, []);
 
   return (
 
     <View style={styles.container}>
-
-      <Modal
-
-        animationType='slide'
-        transparent={true}
-        visible={modal}
-      
-      >
-
-        <View
-          style={styles.center}
-        >
-
-          <View style={styles.content}>
-
-            <Text>Soy un modal</Text>
-
-            <Button
-
-              title='Cerrar modal'
-              onPress={()=>setModal(!modal)}
-              style={styles.button}
-            
-            />
-
-      
-
-
-          </View>
-
-
-
-        </View>
-
-      </Modal>
-
-      <Button 
-
-        title='Abrir modal'
-        onPress={()=>{setModal(!modal)}}
-        style={styles.button}
-      />
-
-    </View>   
+    
+  </View>
 
   );
 }
 
 const styles = StyleSheet.create({
 
-    content: {
+    container: {
 
-      backgroundColor
-
-
-
-    },
-
-      container: {
       flex: 1,
       backgroundColor: 'yellow',
       alignItems: 'center',
       justifyContent: 'center',
-    },
-
-    center: {
-
-      flex: 1,
-      alignItems: 'center',
-      justifyContent: 'center',
-
     },
 
 })
